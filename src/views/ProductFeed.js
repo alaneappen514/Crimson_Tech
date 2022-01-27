6ca3176bc18ce04d1547eb50d47ac02e0eb83ec8
Alan Eappen <eappenalan123@gmail.com>
2022-01-26 19:25:03 -0500

    rendered responsive product feed

--- src/views/ProductFeed.js
+++ src/views/ProductFeed.js
@@ -1,58 +1,45 @@
 import React from "react";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 import { useSelector } from "react-redux";
-
-const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 4,
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1290 },
-    items: 3,
-    partialVisibilityGutter: 10,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 570 },
-    items: 2,
-    partialVisibilityGutter: 10,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-    partialVisibilityGutter: 20,
-  },
-};
 
 const ProductFeed = () => {
   const products = useSelector((state) => state.allProducts.products);
-  const renderProducts = products[0]?.map((product) => {
-    return (
-      <div
-        className=" w-60 md:w-80 lg:w-96  bg-white-smoke rounded-3xl mt-12 drop-shadow-lg"
-        key={product.id}
-      >
-        <div className="text-center py-2 px-10 lg:py-4  lg:px-16">
-          {" "}
-          <img src={product.image} alt="product-image"></img>
-          <h2 className="lg:text-xl font-bold">{product.title}</h2>
-          <div>x x x x x 278</div>
-          <hr className="my-3 "></hr>
-          <h2 className="lg:text-xl font-extrabold">{product.price}</h2>
-          <button className="border-4 border-crimson-red hover:bg-crimson-red hover:text-white-smoke my-3 px-4 py-3 text-xs md:text-base">
-            Buy Now
-          </button>
-        </div>
-      </div>
-    );
-  });
+
   return (
-    <div>
-      <Carousel partialVisible={true} responsive={responsive} ssr={true}>
-        <>{renderProducts}</>
-      </Carousel>
+    <div className="grid grid-flow-row-dense xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+      {products.length > 0 ? (
+        <>
+          {products[0]?.map((product) => {
+            return (
+              <div
+                className="flex flex-col items-center m-5 p-5 bg-white rounded-3xl mt-12 drop-shadow-lg"
+                key={product.id}
+              >
+                {" "}
+                <img
+                  className=" h-52 w-52 object-contain"
+                  src={product.image}
+                  alt="product-image"
+                ></img>
+                <h2 className="lg:text-xl text-lg font-bold mt-4 line-clamp-1">
+                  {product.title}
+                </h2>
+                <div className="text-center">
+                  <div>x x x x x 278</div>
+                  <hr className="my-3 "></hr>
+                  <h2 className="lg:text-xl text-lg font-extrabold">
+                    ${product.price}
+                  </h2>
+                  <button className="border-4 border-crimson-red hover:bg-crimson-red hover:text-white-smoke my-3 px-4 py-3 text-sm md:text-base">
+                    Buy Now
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </>
+      ) : (
+        <p>hello</p>
+      )}
     </div>
   );
 };
